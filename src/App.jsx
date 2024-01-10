80e693161ad0360690f6710cb533a009bae69ce0
pablosanchez <pablosanchezperianez83@gmail.com>
2024-01-10 14:50:53 +0100

    Add element functionality added to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import Creador from './components/Creador';
 import Lista from './components/Lista';
 import Elemento from './components/Elemento';
 
 function App() {
+  const [listaDatos, setListaDatos] = useState([]);
+
+  function handleAdd(tarea) {
+    setListaDatos((prevList) => {
+      const newList = [...prevList, tarea];
+      return newList;
+    });
+  }
   return (
     <section className='p-10 max-w-6xl mx-auto'>
-      <Creador/>
+      <Creador onCreate={handleAdd} />
       <Lista>
-        {/* pensar en forma de añadir varios elementos */}
-        <Elemento data={'test text'} />
-        <Elemento data={'test text'} />
-        <Elemento data={'test text'} />
-        <Elemento data={'test text'} />
+        {listaDatos.map((tarea, index) => (
+          <Elemento data={tarea} key={index} />
+        ))}
       </Lista>
     </section>
   )
