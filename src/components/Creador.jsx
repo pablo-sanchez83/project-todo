d94cfc26e006ae85c9224e82e9a309699f9a0049
pablosanchez <pablosanchezperianez83@gmail.com>
2024-01-10 14:38:13 +0100

    Added functionality to Creador component

--- src/components/Creador.jsx
+++ src/components/Creador.jsx
@@ -1,13 +1,26 @@
-export default function Creador() {
+import { useState } from "react";
+
+export default function Creador({ onCreate }) {
+    const [texto, setTexto] = useState('');
+
+    const handleButtonClick = () => {
+        if (texto !== "") {
+            onCreate(texto);
+            setTexto('');
+        }
+    };
     return (
         <section className="flex justify-center p-10 bg-gray-800 max-w-6xl">
             <input
                 type="text"
                 className="w-10/12 px-4 py-3 rounded-xl mr-2 font-sans font-semibold text-lg uppercase overflow-x-auto focus:outline-none focus:ring focus:border-blue-300"
                 placeholder="Nueva tarea..."
+                value={texto}
+                onChange={(e) => setTexto(e.target.value)}
             />
             <button
                 className="w-2/12 bg-green-500 hover:bg-green-600 rounded-xl text-white text-2xl font-bold transition duration-300 focus:outline-none focus:ring focus:border-blue-300"
+                onClick={handleButtonClick}
             >
                 Añadir
             </button>
