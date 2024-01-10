ed36d8e11a2ae34020cdb58a4f616f7798a7fd47
pablo-sanchez83 <149807125+pablo-sanchez83@users.noreply.github.com>
2024-01-10 17:33:50 +0100

    Final fix on Creador, i hope

--- src/components/Creador.jsx
+++ src/components/Creador.jsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 export default function Creador({ onCreate }) {
     const [texto, setTexto] = useState('');
 
-    const handleButtonClick = () => {
+    const addTask = () => {
         if (texto !== "") {
             onCreate(texto);
             setTexto('');
         }
     };
+
     return (
         <section className="flex justify-center p-10 bg-gray-800 max-w-6xl">
             <input
@@ -18,16 +19,15 @@ export default function Creador({ onCreate }) {
                 placeholder="Nueva tarea..."
                 value={texto}
                 onChange={(e) => setTexto(e.target.value)}
-                onKeyDown={
-                    (e) => {
-                        if (e.key === "Enter") {
-                            handleButtonClick()
-                        }
-                    }}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        addTask();
+                    }
+                }}
             />
             <button
                 className="w-2/12 bg-green-500 hover:bg-green-600 rounded-xl text-white text-2xl font-bold transition duration-300 focus:outline-none focus:ring focus:border-blue-300"
-                onClick={handleButtonClick}
+                onClick={addTask}
             >
                 Añadir
             </button>
